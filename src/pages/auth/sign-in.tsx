69363553463774e9aa4e67fcc75638713613ca86
Bruno Souza <brunoanunciacaosouza@gmail.com>
2024-10-22 17:03:37 -0300

    pagina cadastro de restaurantes

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -6,6 +6,7 @@ import { toast } from "sonner";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
 
 const signInForm = z.object({
   email: z.string().email(),
@@ -36,9 +37,13 @@ export function SignIn() {
       <Helmet title="Login" />
 
       <div className="p-8">
+        <Button variant="ghost" asChild className="absolute right-8 top-8">
+          <Link to="/sign-up">Novo estabelecimento</Link>
+        </Button>
+
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-clip">
-            <h1 className="text-2xl font-semibold capitalize tracking-tight">
+            <h1 className="text-2xl font-semibold tracking-tight">
               Acessar painel
             </h1>
             <p className="text-sm text-muted-foreground">
@@ -57,7 +62,7 @@ export function SignIn() {
               type="submit"
               onClick={handleSubmit(handleSignIn)}
             >
-              Acessar Painel
+              Acessar painel
             </Button>
           </form>
         </div>

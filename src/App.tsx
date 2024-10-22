59952df237c64744334f1eb6b457b3349a3dca3b
Bruno Souza <brunoanunciacaosouza@gmail.com>
2024-10-22 13:58:10 -0300

    configurando eslint e prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { Button } from "./components/ui/button";
+
 export function App() {
   return (
     <>
-      <h1>Pizza Shop</h1>
+      <div className="flex h-screen items-center justify-center">
+        <Button>Enviar</Button>
+      </div>
     </>
   );
 }

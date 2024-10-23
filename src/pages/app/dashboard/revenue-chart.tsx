cdb5c14d58fd983aacb089019b2e8a43c60d0fcc
Bruno Souza <brunoanunciacaosouza@gmail.com>
2024-10-23 13:34:29 -0300

    gráfico receita no periodo

---
+++ src/pages/app/dashboard/revenue-chart.tsx
@@ -0,0 +1,69 @@
+import colors from "tailwindcss/colors";
+
+import {
+  ResponsiveContainer,
+  LineChart,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Line,
+  Tooltip,
+} from "recharts";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+const data = [
+  { data: "10/10", revenue: 1400 },
+  { data: "15/10", revenue: 1200 },
+  { data: "18/10", revenue: 900 },
+  { data: "20/10", revenue: 1600 },
+  { data: "24/10", revenue: 2400 },
+  { data: "30/10", revenue: 2000 },
+];
+
+export function RevenueChart() {
+  return (
+    <Card className="col-span-6">
+      <CardHeader className="flex-row items-center justify-between pb-8">
+        <div className="space-y-1">
+          <CardTitle className="text-base font-medium">
+            Receite no período
+          </CardTitle>
+          <CardDescription>Receita diária no período</CardDescription>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <ResponsiveContainer width="100%" height={240}>
+          <LineChart data={data} style={{ fontSize: 12 }}>
+            <XAxis dataKey="data" tickLine={false} axisLine={false} dy={16} />
+
+            <YAxis
+              stroke="#888"
+              axisLine={false}
+              tickLine={false}
+              width={80}
+              tickFormatter={(value: number) =>
+                value.toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                })
+              }
+            />
+            <Line
+              type="linear"
+              stroke={colors.violet["500"]}
+              strokeWidth={3}
+              dataKey="revenue"
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </CardContent>
+    </Card>
+  );
+}

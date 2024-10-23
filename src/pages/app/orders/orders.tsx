68c38b1ea8a648e036fae7376f0652c29509b586
Bruno Souza <brunoanunciacaosouza@gmail.com>
2024-10-23 11:58:13 -0300

    componente de paginação

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -9,40 +9,43 @@ import {
 } from "@/components/ui/table";
 import { OrderTableRow } from "./order-table-row";
 import { OrderTableFilters } from "./order-table-filters";
+import { Pagination } from "@/components/pagination";
 
 export function Orders() {
   return (
     <>
       <Helmet title="Pedidos" />
 
-      <div className="mb-2 flex flex-col gap-4">
+      <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
-      </div>
 
-      <div className="space-y-2.5">
-        <OrderTableFilters />
+        <div className="space-y-2.5">
+          <OrderTableFilters />
 
-        <div className="rounded-md border">
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead className="w-[64px]"></TableHead>
-                <TableHead className="w-[140px]">Identificador</TableHead>
-                <TableHead className="w-[180px]">Realizado há</TableHead>
-                <TableHead className="w-[140px]">Status</TableHead>
-                <TableHead>Cliente</TableHead>
-                <TableHead className="w-[140px]">Total de pedido</TableHead>
-                <TableHead className="w-[164px]"></TableHead>
-                <TableHead className="w-[132px]"></TableHead>
-              </TableRow>
-            </TableHeader>
+          <div className="rounded-md border">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[64px]"></TableHead>
+                  <TableHead className="w-[140px]">Identificador</TableHead>
+                  <TableHead className="w-[180px]">Realizado há</TableHead>
+                  <TableHead className="w-[140px]">Status</TableHead>
+                  <TableHead>Cliente</TableHead>
+                  <TableHead className="w-[140px]">Total de pedido</TableHead>
+                  <TableHead className="w-[164px]"></TableHead>
+                  <TableHead className="w-[132px]"></TableHead>
+                </TableRow>
+              </TableHeader>
 
-            <TableBody>
-              <OrderTableRow />
-              <OrderTableRow />
-              <OrderTableRow />
-            </TableBody>
-          </Table>
+              <TableBody>
+                <OrderTableRow />
+                <OrderTableRow />
+                <OrderTableRow />
+              </TableBody>
+            </Table>
+          </div>
+
+          <Pagination pageIndex={0} totalCount={5} perPage={10} />
         </div>
       </div>
     </>

d6cd0f7f4497d3caac9577acd5b8e80e068bb733
Bruno Souza <brunoanunciacaosouza@gmail.com>
2024-10-30 14:31:19 -0300

    detalhes do pedido

--- frontend/src/pages/app/orders/order-table-row.tsx
+++ frontend/src/pages/app/orders/order-table-row.tsx
@@ -9,6 +9,7 @@ import { Dialog, DialogTrigger } from "@/components/ui/dialog";
 import { OrderStatus } from "@/components/order-status";
 
 import { OrderDetails } from "./oder-details";
+import { useState } from "react";
 
 export interface OrderTableRowProps {
   order: {
@@ -21,10 +22,12 @@ export interface OrderTableRowProps {
 }
 
 export function OrderTableRow({ order }: OrderTableRowProps) {
+  const [isDetailsOpen, setIsDetailOpen] = useState(false);
+
   return (
     <TableRow>
       <TableCell>
-        <Dialog>
+        <Dialog open={isDetailsOpen} onOpenChange={setIsDetailOpen}>
           <DialogTrigger asChild>
             <Button variant="outline" size="xs">
               <Search className="h-3 w-3" />
@@ -32,33 +35,40 @@ export function OrderTableRow({ order }: OrderTableRowProps) {
             </Button>
           </DialogTrigger>
 
-          <OrderDetails />
+          <OrderDetails orderId={order.orderId} open={isDetailsOpen} />
         </Dialog>
       </TableCell>
+
       <TableCell className="font-mono text-xs font-medium">
         {order.orderId}
       </TableCell>
+
       <TableCell className="text-muted-foreground">
         {formatDistanceToNow(order.createdAt, {
           locale: ptBR,
           addSuffix: true,
         })}
       </TableCell>
+
       <TableCell>
         <OrderStatus status={order.status} />
       </TableCell>
+
       <TableCell className="font-medium">{order.customerName}</TableCell>
+
       <TableCell className="font-medium">
         {order.total.toLocaleString("pt-BR", {
           style: "currency",
           currency: "BRL",
         })}
       </TableCell>
+
       <TableCell>
         <Button variant="outline" size="xs">
           <ArrowRight className="mr-2 h-3 w-3" /> Aprovar
         </Button>
       </TableCell>
+
       <TableCell>
         <Button variant="ghost" size="xs">
           <X className="mr-2 h-3 w-3" /> Cancelar
